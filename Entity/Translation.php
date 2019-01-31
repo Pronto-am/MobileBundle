80195a039bd582e2f421471897acb5116ddee7fe
Thomas Roovers <troovers@e-sites.nl>
2019-01-31 14:34:27 +0100

    Max length for columns with indices

--- Entity/Translation.php
+++ Entity/Translation.php
@@ -23,7 +23,7 @@ class Translation implements ApiEntityInterface
 	 * @var string $id
 	 *
 	 * @ORM\Id
-	 * @ORM\Column(type="string")
+	 * @ORM\Column(type="string", length=191)
 	 * @Groups({"Translation"})
 	 */
 	private $id;
