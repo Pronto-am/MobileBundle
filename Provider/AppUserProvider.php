da142d23171b262f53b14f052ff2208ca15e0856
Thomas Roovers <troovers@e-sites.nl>
2019-10-30 15:33:55 +0100

    - Added method to retrieve client by client_id and client_secret - Retrieving AppUser based on email address and application

--- Provider/AppUserProvider.php
+++ Provider/AppUserProvider.php
@@ -3,7 +3,14 @@
 namespace Pronto\MobileBundle\Provider;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use FOS\OAuthServerBundle\Model\AccessTokenManagerInterface;
+use Pronto\MobileBundle\Entity\Application;
 use Pronto\MobileBundle\Entity\AppUser;
+use Pronto\MobileBundle\Exceptions\ApiException;
+use Pronto\MobileBundle\Utils\Responses\ErrorResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -11,71 +18,130 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class AppUserProvider implements UserProviderInterface
 {
-	/**
+    /**
      * @var EntityManagerInterface $entityManager
      */
-	protected $entityManager;
+    protected $entityManager;
 
-	/**
-	 * AppUserProvider constructor.
-	 * @param EntityManagerInterface $entityManager
-	 */
-	public function __construct(EntityManagerInterface $entityManager)
-	{
-		$this->entityManager = $entityManager;
-	}
+    /**
+     * @var Request $request
+     */
+    private $request;
 
-	/**
-	 * Get user by email address
-	 *
-	 * @param string $email
-	 * @return UserInterface
-	 */
-	public function loadUserByUsername($email): UserInterface
-	{
-		/** @var AppUser $user */
-		$user = $this->entityManager->getRepository(AppUser::class)->findOneBy([
-			'email'           => $email,
-			'activated'       => true
-		]);
+    /**
+     * AppUserProvider constructor.
+     * @param EntityManagerInterface $entityManager
+     * @param RequestStack $requestStack
+     */
+    public function __construct(EntityManagerInterface $entityManager, RequestStack $requestStack)
+    {
+        $this->entityManager = $entityManager;
+        $this->request = $requestStack->getCurrentRequest();
+    }
 
-		if ($user === null) {
-			$message = sprintf('Unable to find an active AppUser identified by "%s".', $email);
+    /**
+     * Get user by email address
+     *
+     * @param string $email
+     * @return UserInterface
+     * @throws ApiException
+     */
+    public function loadUserByUsername($email): UserInterface
+    {
+        $application = null;
 
-			throw new UsernameNotFoundException($message, 404);
-		}
+        // Check application
+        if ($this->request->request->has('client_id') && $this->request->request->has('client_secret')) {
+            $application = $this->determineApplication($this->request->request->get('client_id'), $this->request->request->get('client_secret'));
 
-		return $user;
-	}
+        } elseif ($this->request->headers->has('Authorization')) {
+            // Split header value
+            try {
+                [, $basic] = explode(' ', $this->request->headers->get('Authorization'));
+                [$clientId, $clientSecret] = explode(':', base64_decode($basic));
+                $application = $this->determineApplication($clientId, $clientSecret);
 
-	/**
-	 * Refresh the user
-	 *
-	 * @param UserInterface|AppUser $user
-	 * @return null|object|UserInterface
-	 */
-	public function refreshUser(UserInterface $user)
-	{
-		$class = get_class($user);
+            } catch (Exception $exception) {
+                $this->invalidAuthorization();
+            }
+        }
 
-		if (!$this->supportsClass($class)) {
-			throw new UnsupportedUserException(
-				sprintf(
-					'Instances of "%s" are not supported.',
-					$class
-				)
-			);
-		}
+        if ($application === null) {
+            $this->invalidAuthorization();
+        }
 
-		return $this->entityManager->getRepository(AppUser::class)->find($user->getId());
-	}
+        /** @var AppUser $user */
+        $user = $this->entityManager->getRepository(AppUser::class)->findOneBy([
+            'email'       => $email,
+            'activated'   => true,
+            'application' => $application,
+        ]);
 
-	/**
-	 * @param string $class
-	 * @return bool
-	 */
-	public function supportsClass($class): bool
-	{
-		return AppUser::class === $class || is_subclass_of($class, AppUser::class);
-	}
+        if ($user === null) {
+            $message = sprintf('Unable to find an active AppUser identified by "%s".', $email);
+
+            throw new UsernameNotFoundException($message, 404);
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param string $clientId
+     * @param string $clientSecret
+     * @return Application|null
+     * @throws ApiException
+     */
+    private function determineApplication(string $clientId, string $clientSecret): ?Application
+    {
+        try {
+            [, $randomId] = explode('_', $clientId);
+
+            return $this->entityManager->getRepository(Application::class)->findByOAuthCredentials($randomId, $clientSecret);
+        } catch (Exception $exception) {
+            $this->invalidAuthorization();
+        }
+    }
+
+    /**
+     * @throws ApiException
+     */
+    private function invalidAuthorization()
+    {
+        $response = new ErrorResponse(ErrorResponse::INVALID_AUTHORIZATION_TOKEN);
+        $response->create();
+
+        throw new ApiException($response);
+    }
+
+    /**
+     * Refresh the user
+     *
+     * @param UserInterface|AppUser $user
+     * @return null|object|UserInterface
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        $class = get_class($user);
+
+        if (!$this->supportsClass($class)) {
+            throw new UnsupportedUserException(
+                sprintf(
+                    'Instances of "%s" are not supported.',
+                    $class
+                )
+            );
+        }
+
+        return $this->entityManager->getRepository(AppUser::class)->find($user->getId());
+    }
+
+    /**
+     * @param string $class
+     * @return bool
+     */
+    public function supportsClass($class): bool
+    {
+        return AppUser::class === $class || is_subclass_of($class, AppUser::class);
+    }
 }
